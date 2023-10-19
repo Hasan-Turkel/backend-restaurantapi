925e082f3c8d462473c45d50b6875188dda44434
hasan <mmhotmail>
2023-10-19 14:45:46 +0300

    logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -10,5 +10,5 @@ const now = new Date()
 const today = now.toISOString().split('T')[0]
 
 module.exports = morgan('combined', {
-    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a' })
+    stream: fs.createWriteStream(`./${today}.log`, { flags: 'a' })
 })
